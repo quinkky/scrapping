954ccf444f401b626ef1d028f2e3c9f8f385f2ce
Roman <r9mawka@gmail.com>
2023-09-14 23:41:05 +0200

    załóż konto

--- main.py
+++ main.py
@@ -22,15 +22,34 @@ async def main():
     for index, element in enumerate(elements):
         if index == 0:
             _id = await page.evaluate('element => element.id', element)
-            await page.type("input[id=" + _id + "]","maciek")
+            await page.type("input[id=" + str(_id) + "]","maciek")
+            print("input[id=" + _id + ']')
         if index == 1:
             _id = await page.evaluate('element => element.id', element)
-            await page.type("input[id=" + _id + "]", "mioduszewski")
+            await page.type("input[id=" + str(_id) + "]", "mioduszewski")
+            print("input[id=" + _id + ']')
     await page.waitForSelector("#birthdayDay")
     await page.type("#birthdayDay","10")
     await page.click('.icon-arrow-right-full')
     await page.click(".account-select__options__item")
+    await page.type('#birthdayYear',"1990")
+    sleep(1)
+    await page.click('#mainApp > div > div > div > div > div.register-page-wrapper > div > form > div.register-form__inputs > div.account-input-container.account-select > div.icon-arrow-right-full')
+    sleep(1)
+    await page.click('.account-select__options__item')
+    await page.waitForSelector('.register-form__inputs-mail > div > input')
+    await page.type('.register-form__inputs-mail > div > input','kashgsdg2312')
+    await page.type('#password',"Adfgkdfgad1232.")
+    await page.type('#rePassword',"Adfgkdfgad1232.")
+    # await page.waitForSelector("input[name="+ "agreementacceptAll-acceptAll" + "]")
+    await page.click("div.checkbox-label")
+    sleep(3)
+    await page.waitForSelector('div.register-form__information > button')
+    await page.click('div.register-form__information > button')
+    await page.click('div.register-form__information > button')
+    await page.click('div.register-form__information > button')
     
+    # await page.click('button[type=submit]')
 
 
 
@@ -38,7 +57,8 @@ async def main():
 
 
 
-    sleep(5)
+
+    sleep(10)
         # await browser.close()
 
 asyncio.get_event_loop().run_until_complete(main())
