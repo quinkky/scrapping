b2cb042e7af329f9bcfb7d92922ef59db6c33c2f
Roman <r9mawka@gmail.com>
2023-09-14 22:54:54 +0200

    month

--- main.py
+++ main.py
@@ -7,33 +7,38 @@ from time import sleep
 
 registerUrl = 'https://konto-pocztowe.interia.pl/#/nowe-konto/darmowe'
 async def main():
-    try:
-        #entering the page
-        browser = await launch(headless = False)
-        page = await browser.newPage()
-        await page.goto(registerUrl)
-        #rodo pass
-        rodoButton = await page.querySelector('.rodo-popup-agree')
-        await rodoButton.click()
-        #search and type name and surname
-        elements = await page.querySelectorAll('.register-form__inputs > div > input')
-        for index, element in enumerate(elements):
-            _id = await page.evaluate('element => element.id',element)
-            print(index)
-            if index == 0:
-                await page.type("input[id=" + _id + "]","maciek")
-            if index == 1:
-                await page.type("input[id=" + _id + "]", "mioduszewski")
-        print(1)
+
+    #entering the page
+    browser = await launch(headless = False)
+    page = await browser.newPage()
+    await page.goto(registerUrl)
+    #rodo pass
+    await page.waitForSelector('.rodo-popup-agree')
+    rodoButton = await page.querySelector('.rodo-popup-agree')
+    await rodoButton.click()
+    #search and type name and surname
+    await page.waitForSelector('.register-form__inputs > div > input')
+    elements = await page.querySelectorAll('.register-form__inputs > div > input')
+    for index, element in enumerate(elements):
+        if index == 0:
+            _id = await page.evaluate('element => element.id', element)
+            await page.type("input[id=" + _id + "]","maciek")
+        if index == 1:
+            _id = await page.evaluate('element => element.id', element)
+            await page.type("input[id=" + _id + "]", "mioduszewski")
+    await page.waitForSelector("#birthdayDay")
+    await page.type("#birthdayDay","10")
+    await page.click('.icon-arrow-right-full')
+    await page.click(".account-select__options__item")
+    
 
 
 
 
-        sleep(5)
+
+
+
+    sleep(5)
         # await browser.close()
 
-
-
-    except:
-        print("Wystapil blad")
 asyncio.get_event_loop().run_until_complete(main())
